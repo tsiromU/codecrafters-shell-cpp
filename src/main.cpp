de91c49c69935f0ae5c6ff38173e516ffd32fa03
Denis <denischirets@gmail.com>
2025-05-03 13:03:26 +0300

    implementing type

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,17 @@ int main() {
       std::cout << input.substr(5) << std::endl;
       continue;
     }
+    if(input.find_first_of("type ") == 0){
+      input = input.substr(5);
+      if(input == "echo"){
+        std::cout << "echo is a shell builtin" << std::endl;
+      } else if (input == "exit"){
+        std::cout << "exit is a shell builtin" << std::endl;
+      } else{
+        std::cout << input << ": not found" << std::endl;
+      }
+      continue;
+    }
     std::cout << input << ": command not found" << std::endl;
   }
 }
